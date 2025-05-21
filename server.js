6f9b40337736732e5f62f87cc0bdbefa540b319e
Luis Silva <luis.silva.fernando@outlook.com>
2025-05-20 21:00:20 -0400

    Estudando variáveis em Javascript

--- server.js
+++ server.js
@@ -9,3 +9,41 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+//variável
+let carro = {};
+
+// constante, array com objetos
+const tarefas = [
+  {
+    id:"1",
+    texto:"Tirar o lixo",
+    completo: false
+  },
+  {
+    id:"2",
+    texto:"Varrer a chão",
+    completo: true
+  },
+  {
+    id:"3",
+    texto:"Lavar o carro",
+    completo: false
+  }
+];
+
+//Objeto
+carro = {
+  marca:"Toyata",
+  modelo:"Civic",
+  ano: 2025,
+  cor : ["Azul","Rosa","Verde"],
+  acessorios:{
+    aro:14,
+    airbags:"Tecido Veludo",
+    sensoreDeChuva: true,
+    sensoresDeTerromoto: false,
+  }
+}
+
+console.log(tarefas[1].texto.ma);
--- src/playgroundJs.js
+++ src/playgroundJs.js
@@ -1,6 +1,23 @@
-function PressBtn(){
 
-    alert("Eu manjo de Java...");
-    alert("Script");
-    return
+
+let name = prompt("Qual é o seu nome?", "");
+
+function PressBtn(){
+    let age = prompt("Qual é a sua idade?", 0);
+    let alergico = confirm("É alérgico a algo?");
+    let alergia ="";
+    
+    alert(`Bem vindo ${name}`);
+    alert( ' ' == false );
+    if (!alergico && age >=18) {
+        alergia = prompt("Alérgico ao que?", "");
+    }else{
+        alert("É necessário ter a idade mínimo 18, para poder se alistar");
+    }
+
+}
+
+
+function PressBtn2(){
+    let name = prompt("Sobrenome?", "");
 }
